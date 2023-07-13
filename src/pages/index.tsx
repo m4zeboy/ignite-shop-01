c90b1ccea931bc5786e7299fac499a7826d29d6a
m4zeboy <moises775azevedo@gmail.com>
2023-07-13 19:37:21 -0300

    estilos globais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,18 @@
+import { styled } from "@/styles"
+
+const Button = styled('button', {
+  backgroundColor: "$green500",
+  borderRadius: 8,
+  border: 0,
+  padding: "2px 10px",
+  span: {
+    fontWeight: 'bold'
+  },
+  "&:hover": {
+    filter: "brightness(0.8)"
+  }
+})
+
 export default function Home() {
-  return <h1>Hello world</h1>
+  return <Button><span>Teste</span>   Enviar</Button>
 }
