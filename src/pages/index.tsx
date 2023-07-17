d60da7472400a2ce21ec42d5cfcb5642338fb191
m4zeboy <moises775azevedo@gmail.com>
2023-07-17 15:22:04 -0300

    Estrutura da home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,29 @@
 import { styled } from "@/styles"
+import { HomeContainer, Product } from "@/styles/pages/home"
+import Image from "next/image"
 
-const Button = styled('button', {
-  backgroundColor: "$green500",
-  borderRadius: 8,
-  border: 0,
-  padding: "2px 10px",
-  span: {
-    fontWeight: 'bold'
-  },
-  "&:hover": {
-    filter: "brightness(0.8)"
-  }
-})
+import camiseta1 from '../assets/shirt1.png';
+import camiseta2 from '../assets/shirt2.png';
+import camiseta3 from '../assets/shirt3.png';
 
 export default function Home() {
-  return <Button><span>Teste</span>   Enviar</Button>
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} alt="" width={520} height={480}/>
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 100,00</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} alt="" width={520} height={480}/>
+        <footer>
+          <strong>Camiseta XI</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  ) 
 }
