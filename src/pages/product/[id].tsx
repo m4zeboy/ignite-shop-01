267afce22f1ab4d88122babd0e9327323e143686
m4zeboy <moises775azevedo@gmail.com>
2023-07-30 15:02:36 -0300

    Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,22 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product";
+import Image from "next/image";
 import { useRouter } from "next/router";
 
 export default function Product() {
   const { query } = useRouter();
 
-  return <h1>{JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer>
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,99</span>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit saepe debitis magnam quidem nostrum, voluptates amet quo quas, optio libero repellat sunt voluptatum ipsum natus dolorum totam dignissimos accusantium iure.</p>
+
+        <button>Comprar Agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
