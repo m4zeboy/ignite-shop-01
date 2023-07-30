10a634fee39470fcf1ec8df6b30606013d7217fa
m4zeboy <moises775azevedo@gmail.com>
2023-07-30 15:21:13 -0300

    Carregando dados do produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,22 +1,63 @@
+import { stripe } from "@/lib/stripe";
 import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product";
+import { GetStaticProps } from "next";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import Stripe from "stripe";
 
-export default function Product() {
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+  }
+}
+
+export default function Product({ product }: ProductProps) {
   const { query } = useRouter();
 
   return (
     <ProductContainer>
       <ImageContainer>
+        <Image src={product.imageUrl} width={300} height={400} alt=""></Image>
       </ImageContainer>
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79,99</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit saepe debitis magnam quidem nostrum, voluptates amet quo quas, optio libero repellat sunt voluptatum ipsum natus dolorum totam dignissimos accusantium iure.</p>
+        <p>{product.description}</p>
 
         <button>Comprar Agora</button>
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+  const productId = params.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price'],
+  })
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat("pt-BR", {
+          style: "currency",
+          currency: "BRL"
+        }).format(price.unit_amount / 100)
+      },
+      description: product.description,
+
+    },
+    revalidate: 60 * 60 * 1 // 1 hour
+  }
 }
